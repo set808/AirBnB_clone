d0f4b8c1e736a47f43fb9480b05d4b4bc7aaa99f
Sidney Burkett <sidneyriffic@gmail.com>
2018-06-12 22:10:57 -0700

    Add a non-empty file storage reload test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -68,7 +68,7 @@ class TestFileStorage(unittest.TestCase):
         #print(instances_dic[key])
         self.assertIsNotNone(instances_dic[key])
 
-    def test_reload(self):
+    def test_reload_empty(self):
         """
         Tests method: reload (reloads objects from string file)
         """
@@ -83,3 +83,17 @@ class TestFileStorage(unittest.TestCase):
             for line in r:
                 self.assertEqual(line, "{}")
         self.assertIs(a_storage.reload(), None)
+
+    def test_reload(self):
+        """
+        Tests method: reload (reloads objects from string file)
+        """
+        a_storage = FileStorage()
+        try:
+            os.remove("file.json")
+        except:
+            pass
+        with open("file.json", "w") as f:
+            f.write("""{"Place.5e817d8e-f407-4baa-8fcc-8326e6bca20e": {"id": "5e817d8e-f407-4baa-8fcc-8326e6bca20e", "created_at": "2018-06-12T21:51:53.933617", "updated_at": "2018-06-12T21:51:53.933617", "__class__": "Place"}, "State.0a921bcb-7b08-4167-b680-c414f9656844": {"id": "0a921bcb-7b08-4167-b680-c414f9656844", "created_at": "2018-06-12T21:51:56.316635", "updated_at": "2018-06-12T21:51:56.316635", "__class__": "State"}, "City.432003f2-dd04-4cfe-9cea-d4ffb2f32b98": {"id": "432003f2-dd04-4cfe-9cea-d4ffb2f32b98", "created_at": "2018-06-12T21:51:58.312707", "updated_at": "2018-06-12T21:51:58.312707", "__class__": "City"}, "Amenity.01675e9c-5355-423d-a4e8-7bca9f28f2a7": {"id": "01675e9c-5355-423d-a4e8-7bca9f28f2a7", "created_at": "2018-06-12T21:52:02.041651", "updated_at": "2018-06-12T21:52:02.041651", "__class__": "Amenity"}, "Review.e5d7eff1-aeda-4a5b-bb48-22abd6cd785d": {"id": "e5d7eff1-aeda-4a5b-bb48-22abd6cd785d", "created_at": "2018-06-12T21:52:04.868774", "updated_at": "2018-06-12T21:52:04.868774", "__class__": "Review"}, "User.596de9fc-c740-4359-8eda-187f8e12d668": {"id": "596de9fc-c740-4359-8eda-187f8e12d668", "created_at": "2018-06-12T21:56:44.156849", "updated_at": "2018-06-12T21:56:44.156849", "__class__": "User"}, "BaseModel.ec566da4-cf8c-4a6d-82e1-98809963f8e2": {"id": "ec566da4-cf8c-4a6d-82e1-98809963f8e2", "created_at": "2018-06-12T21:56:47.740809", "updated_at": "2018-06-12T21:56:47.740809", "__class__": "BaseModel"}}""")
+        a_storage.reload()
+        self.assertEqual(a_storage.all(), json.loads("""{"Place.5e817d8e-f407-4baa-8fcc-8326e6bca20e": {"id": "5e817d8e-f407-4baa-8fcc-8326e6bca20e", "created_at": "2018-06-12T21:51:53.933617", "updated_at": "2018-06-12T21:51:53.933617", "__class__": "Place"}, "State.0a921bcb-7b08-4167-b680-c414f9656844": {"id": "0a921bcb-7b08-4167-b680-c414f9656844", "created_at": "2018-06-12T21:51:56.316635", "updated_at": "2018-06-12T21:51:56.316635", "__class__": "State"}, "City.432003f2-dd04-4cfe-9cea-d4ffb2f32b98": {"id": "432003f2-dd04-4cfe-9cea-d4ffb2f32b98", "created_at": "2018-06-12T21:51:58.312707", "updated_at": "2018-06-12T21:51:58.312707", "__class__": "City"}, "Amenity.01675e9c-5355-423d-a4e8-7bca9f28f2a7": {"id": "01675e9c-5355-423d-a4e8-7bca9f28f2a7", "created_at": "2018-06-12T21:52:02.041651", "updated_at": "2018-06-12T21:52:02.041651", "__class__": "Amenity"}, "Review.e5d7eff1-aeda-4a5b-bb48-22abd6cd785d": {"id": "e5d7eff1-aeda-4a5b-bb48-22abd6cd785d", "created_at": "2018-06-12T21:52:04.868774", "updated_at": "2018-06-12T21:52:04.868774", "__class__": "Review"}, "User.596de9fc-c740-4359-8eda-187f8e12d668": {"id": "596de9fc-c740-4359-8eda-187f8e12d668", "created_at": "2018-06-12T21:56:44.156849", "updated_at": "2018-06-12T21:56:44.156849", "__class__": "User"}, "BaseModel.ec566da4-cf8c-4a6d-82e1-98809963f8e2": {"id": "ec566da4-cf8c-4a6d-82e1-98809963f8e2", "created_at": "2018-06-12T21:56:47.740809", "updated_at": "2018-06-12T21:56:47.740809", "__class__": "BaseModel"}}""")
