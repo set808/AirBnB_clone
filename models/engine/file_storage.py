4b87f460932dc4aadd37900bb9e9e809c596e400
Sidney Burkett <sidneyriffic@gmail.com>
2018-06-13 12:27:13 -0700

    Clear dictionary before reloading in file storage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -62,6 +62,7 @@ class FileStorage:
         try:
             with open(FileStorage.__file_path, 'r') as f:
                 dicts = json.load(f)
+                FileStorage.__objects = {}
                 for k, v in dicts.items():
                     obj = FileStorage.class_dict[v['__class__']](**v)
                     FileStorage.__objects[k] = obj
